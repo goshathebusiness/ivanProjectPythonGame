34dc70198b4a2068622e0c6b4888f011bc492ad5
goshathebusiness <imperio091@gmail.com>
2022-07-14 19:34:58 +0300

    pitch engine sound

--- sounds.py
+++ sounds.py
@@ -8,6 +8,7 @@ musicVolume=0.5
 effectVolume=1
 
 soundTest=pyglet.media.load('resources/sounds/sf2gt.mp3', streaming=False)
+engineSound=pyglet.media.load('resources/sounds/engine.wav', streaming=False)
 soundTest.volume=(masterVolume*musicVolume)
 
 window = pyglet.window.Window(width=1200, height=900, caption="Sound test", resizable=False)
@@ -16,22 +17,31 @@ window.set_location(0, 30)
 player=pyglet.media.Player()
 player.volume=masterVolume*musicVolume
 
+up=False
+down=False
+
 @window.event
 def on_key_press(symbol, modifiers):
+    global up, down
+    
     if symbol==key.RIGHT:
-        pass
+        player.queue(engineSound)
+        player.play()
+        player.loop=True
     if symbol==key.LEFT:
         pass
     if symbol==key.SPACE:
         player.queue(soundTest)
         player.play()
     if symbol==key.UP:
-        pass
+        up=True
     if symbol==key.DOWN:
-        pass
+        down=True
 
 @window.event
 def on_key_release(symbol, modifiers):
+    global up, down
+    
     if symbol==key.RIGHT:
         pass
     if symbol==key.LEFT:
@@ -40,12 +50,21 @@ def on_key_release(symbol, modifiers):
         player.pause()
         player.next_source()
     if symbol==key.UP:
-        pass
+        up=False
     if symbol==key.DOWN:
-        pass
+        down=False
+
+def pitch():
+    global up
+    if up==True and player.pitch<2:
+        player.pitch+=0.02
+    elif down==True and player.pitch>1:
+        player.pitch-=0.04
+    if player.pitch<0:
+        player.pitch+=0.01
 
 def update(frame):
-    pass
+    pitch()
     
 
 if __name__ == '__main__':
